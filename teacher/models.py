341c4651c33e13ef2844351a1a353a47e8bd0f3a
Рустам Кадиров <rustam@MacBook-Pro-Rustam.local>
2025-05-08 17:22:05 +0500

    review model added

--- teacher/models.py
+++ teacher/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from student.models import Student
 from django.conf import settings 
 
 class Subject(models.Model):
@@ -15,6 +16,11 @@ class Teacher(models.Model):
     price_per_hous = models.IntegerField()
     work_experience = models.IntegerField()
     avatar = models.ImageField(upload_to='avatars', default='avatar_threshold.png')
+    education = models.CharField(max_length=255, verbose_name="Образование", blank=True)
+    video_intro = models.URLField(blank=True, null=True) 
+    languages = models.CharField(max_length=255, blank=True, verbose_name="Языки преподавания")
+    rating = models.FloatField(default=0.0, null=True, blank=True)
+    review_count = models.PositiveIntegerField(default=0, null=True, blank=True)
     
     def __str__(self):
         return f'{self.user}'
@@ -27,5 +33,15 @@ class ScheduleSlot(models.Model):
     
     def __str__(self):
         return f'{self.date}, {self.time}'
-    
+
+
+class Review(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='reviews')
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True)
+    rating = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.teacher} - {self.rating}⭐"
 # Create your models here.
